57494947037cdc1fa5846cd247c8cabf3673d5b8
Ray Dino <ray.e.dino@gmail.com>
2015-05-07 15:22:13 +0800

    Board class + gitignore

--- models.py
+++ models.py
@@ -1,10 +1,10 @@
-from numpy.random import randint
+from numpy.random import rand, randint
+from numpy import rint, int64
 
 import settings
 
 
 class DNA(object):
-    _dna_sequence = list()
     
     def __init__(self, init_sequence=None):
         if init_sequence is None:
@@ -26,8 +26,7 @@ class DNA(object):
 
 
 class Robby(object):
-    _dna = None
-    
+
     def __init__(self, dna):
         self._dna = dna
 
@@ -40,4 +39,57 @@ class Robby(object):
 
 
 class Board(object):
-    pass
+    
+    def __init__(self):
+        self._board = rint(rand(10, 10)).astype(int64)
+
+    def get_scenario(self, x, y):
+        """Return a base-10 integer that is the equivalent of the 5-digit 
+        base-3 number that corresponds to one of 234 possible scenarios.
+
+        The trit positions mean:
+        North - East - West - South - Current
+        3^4   - 3^3  - 3^2  - 3^1   - 3^0
+
+        Trit value meanings:
+        0 - Empty
+        1 - With a can
+        2 - Edge of board
+
+        Arguments:
+        x -- x-coordinate
+        y -- y-coordinate
+
+        """
+        
+        scenario = [
+            str(self._get_trit(x, y-1)),
+            str(self._get_trit(x+1, y)),
+            str(self._get_trit(x-1, y)),
+            str(self._get_trit(x, y+1)),
+            str(self._get_trit(x, y))
+        ]
+        return base3_to_base10(''.join(scenario))
+
+    def _get_trit(self, x, y):
+        try:
+            if x<0 or y<0:
+                raise Exception
+            return self._board[x, y]
+        except:
+            return 2
+
+    def pickup_can(self, x, y):
+        if self._board[x, y]:
+            self._board[x, y] = 0
+            return True
+        else:
+            return False
+
+
+def base3_to_base10(base3_str):
+    strlen = len(base3_str)
+    base10_int = 0
+    for index, value in enumerate(base3_str):
+        base10_int += int(value) * 3**(strlen-1-index)
+    return base10_int
